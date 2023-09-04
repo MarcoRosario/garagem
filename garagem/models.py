4d6754986991aae7d61780b31718a2fb544b1944
Marco Rosario <marcoantoniorosario.academico@gmail.com>
2023-09-04 09:29:50 -0300

    Editando ano para rodar

--- garagem/models.py
+++ garagem/models.py
@@ -40,9 +40,13 @@ class Veiculo(models.Model):
     marca = models.ForeignKey(
         Marca, on_delete=models.PROTECT, related_name="Veiculo"
 )   
+    ano = models.IntegerField(default=0, null=True, blank=True
+        
+    )
+    
     descricao = models.CharField(max_length=50)
     
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     def __str__(self):
-        return f"{self.marca} ({self.cor} ({self.descricao})"
+        return f"{self.marca}({self.cor} ({self.descricao})"
  
