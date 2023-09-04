8dad19bec36871b83640f065e8a0459772ae1ece
Marco Rosario <marcoantoniorosario.academico@gmail.com>
2023-09-04 09:26:03 -0300

    Criando atributos para veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -3,10 +3,11 @@ from django.db import models
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50,blank=True,null=True)
-    ano_fundacao = models.IntegerField(blank=True, null=True)
+    ano = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return self.nome.upper()
+        return self.nome.upper()      
+  
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=50,blank=True,null=True)
@@ -26,7 +27,6 @@ class Acessório(models.Model):
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
   
-
     def __str__(self):
         return self.descricao
 
@@ -40,3 +40,9 @@ class Veiculo(models.Model):
     marca = models.ForeignKey(
         Marca, on_delete=models.PROTECT, related_name="Veiculo"
 )   
+    descricao = models.CharField(max_length=50)
+    
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    def __str__(self):
+        return f"{self.marca} ({self.cor} ({self.descricao})"
+ 
